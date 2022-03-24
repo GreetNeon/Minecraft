ee657d241a07b22e8e287a99fb433b77857335df
GreetNeon <97176150+GreetNeon@users.noreply.github.com>
2022-03-24 21:11:27 +0000

    Add files via upload

---
+++ minecraft/mine_main.py
@@ -0,0 +1,107 @@
+from tkinter import Scale
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+app = Ursina()
+
+grass_texture = load_texture('assets/grass_block.png')
+stone_texture = load_texture('assets/stone_block.png')
+brick_texture = load_texture('assets/brick_block.png')
+dirt_texture  = load_texture('assets/dirt_block.png')
+sky_texture = load_texture('assets/skybox.png')
+arm_texture = load_texture('assets/arm_texture.png')
+punch_sound = Audio('assets/punch_sound', Loop = False, autoplay = False)
+dirt_sound = Audio('assets/dirt_sound.mp3', loop = False, autoplay = False)
+stone_sound = Audio('assets/stone_sound.mp3', loop = False, autoplay = False)
+background_music = Audio('assets/background.mp4', Loop = True, autoplay = True)
+block_pick = 1
+
+def update():
+    global block_pick
+
+    if held_keys['left mouse'] or held_keys['roght mouse']:
+        hand.active()
+    else:
+        hand.passive()
+
+    if held_keys['1']: block_pick = 1
+    if held_keys['2']: block_pick = 2
+    if held_keys['3']: block_pick = 3
+    if held_keys['4']: block_pick = 4
+
+class Voxel(Button):
+    def __init__(self, position = (0, 0, 0), texture = grass_texture):
+        if position[1] < 0 and texture == grass_texture:
+            texture = dirt_texture
+        super().__init__(
+            parent = scene,
+            position = position,
+            model = 'assets/block',
+            origin_y = 0.5,
+            texture = texture,
+            color = color.color(0, 0, random.uniform(0.9, 1)),
+            scale = 0.5
+        )
+
+    def input(self, key):
+        if self.hovered:
+            if key == 'right mouse down':
+                if block_pick == 1:
+                    voxel = Voxel(position = self.position + mouse.normal, texture = grass_texture)
+                    dirt_sound.play()
+
+                if block_pick == 2:
+                    voxel = Voxel(position = self.position + mouse.normal, texture = stone_texture)
+                    stone_sound.play()
+
+                if block_pick == 3: 
+                    voxel = Voxel(position = self.position + mouse.normal, texture = brick_texture)
+                    stone_sound.play()
+
+                if block_pick == 4: 
+                    voxel = Voxel(position = self.position + mouse.normal, texture = dirt_texture)
+                    dirt_sound.play()
+
+            if key == 'left mouse down':
+                if self.position[1] != -2:
+                    punch_sound.play()
+                    destroy(self)
+
+class Sky(Entity):
+    def __init__(self):
+        super().__init__(
+            parent = scene,
+            model = 'sphere',
+            texture = sky_texture,
+            scale = 150,
+            double_sided = True
+        )
+
+class Hand(Entity):
+    def __init__(self):
+        super().__init__(
+            parent = camera.ui,
+            model = 'assets/arm',
+            texture = arm_texture,
+            scale = 0.2,
+            rotation = Vec3(150, -10, 0),
+            position = Vec2(0.7, -0.6)
+        )
+
+    def active(self):
+        self.position = Vec2(0.3, -0.5)
+
+    def passive(self):
+        self.position = Vec2(0.7, -0.6)
+
+for k in range(20):
+    for i in range(20):
+        for z in range (0, -3, -1):
+            voxel = Voxel(position = (i, z, k))
+
+player = FirstPersonController()
+player.height, player.jump_height, player.jump_up_duration, player.gravity = 1, 1, 0.25
+sky = Sky()
+hand = Hand()
+
+app.run()
